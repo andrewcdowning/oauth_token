b19a3d33a88d111ea4a2a722384ac1b6e8c67d8a
chickenslinger <chickenslinger@users.noreply.github.com>
2019-02-01 12:06:16 -0700

    Add __repr__ method
    Also cleaning up debug statments

--- OctoToken.py
+++ OctoToken.py
@@ -18,16 +18,17 @@ class OctoToken(object):
         self.expires = 0
         self.grant_type = 'client_credentials'
         self.scope = "urn:opc:idm:__myscopes__"
+        
+    def __repr__(self):
+        return '{self.__class__.__name__}({self.client_id},{self.oauth_uri},{self.grant_token})
 
     def get_token(self):
-        #print(self.expires, int(time.time()))
         if self.expires < int(time.time()):
             return self._gen_token()
         else:
             return self.grant_token
 
     def _gen_token(self):
-        #print("call to api!!!")
         body_params = {'grant_type': self.grant_type, 'scope': self.scope}
         self.grant_token = requests.post(self.oauth_uri,
                                          data=body_params,
